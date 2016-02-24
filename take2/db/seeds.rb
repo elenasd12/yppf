92c2c8e050993bba5160659fbc532f52e7b60e27
Elena <elenasd12@gmail.com>
2016-02-24 14:58:37 -0500

    income working, but decimals are not

--- take2/db/seeds.rb
+++ take2/db/seeds.rb
@@ -8,16 +8,21 @@
 Bill.delete_all
 Expense.delete_all
 Income.delete_all
+User.delete_all
 
 5.times do
   Bill.create(userid: "Eden", expenseid: Faker::Commerce.department, day_month: Faker::Number.number(2), month: Faker::Number.number(1), year: Faker::Number.number(4))
 end
 
 5.times do
-  Expense.create(expenseid: Faker:: Commerce.department, userid: "Eden", expensetype: Faker:: Lorem.word, frequency: "biweekly", projvalue: Faker:: Commerce.price, actvalue: Faker:: Commerce.price, percent: Faker:: Number.number(2), month: Faker:: Number.number(1), year: Faker:: Number.number(4))
+  User.create(user: "Eden", email: Faker::Internet.email, password: Faker::Internet.password, state: Faker::Address.state_abbr)
 end
 
 
 5.times do
-  Income.create(userid: "Eden", income: "Money", incometype: "salary", value: Faker:: Number.number, frequency: "biweekly", day_rec: Faker:: Number.number(2), month: Faker:: Number.number(1), year: Faker:: Number.number(4))
+  Expense.create(expenseid: Faker::Commerce.department, userid: "Eden", expensetype: Faker::Lorem.word, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: Faker::Number.number(1), year: Faker::Number.number(4))
+end
+
+5.times do
+  Income.create(userid: "Eden", income: "Money", incometype: "salary", value: "Faker::Commerce.price", frequency: "biweekly", day_rec: Faker::Number.number(2), month: Faker::Number.number(1), year: Faker::Number.number(4))
 end
