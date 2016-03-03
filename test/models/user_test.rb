ec40d532d08a7850f9df55a49157f188e9770245
Mojir Samimi <mojir.s@gmail.com>
2016-03-02 20:10:05 -0500

    test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,10 +7,25 @@ class UserTest < ActiveSupport::TestCase
   
   test "user's email should be valid" do
       user = User.find(1)
-      #user.name="john smith"
-      user.email="mmm@mm.com"
-      #user.encrypted_password="asdwqe"
-      puts user.valid?
-      assert user.invalid?, "user's email is not valid"      
+      
+      user.email="mmm"
+      
+      assert user.valid?,"user's email should be valid"    
+         
+  end
+  
+  test "user should be instance of user" do
+      user = User.new
+     assert_instance_of User,user,"user should be instance of the class User"
+          
+  end
+  
+  test "user's required attributes must not be empty" do
+      user = User.find(1)
+      user.name=""
+      assert user.errors[:name].any?,"name must not be empty"
+      assert user.errors[:email].any?,"email must not be empty"
+      assert user.errors[:encrypted_password].any?,"email must not be empty"
+        
   end
 end
