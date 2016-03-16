7c33b2ef16ebe54ce0d19a0508f70d6c126a3088
Eden Shoshan <eshoshan@brandeis.edu>
2016-03-16 11:45:09 -0400

    seed data current month & budget page changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,19 @@ User.delete_all
 User.create(email: "yppf2016@gmail.com", state: "MA", name: "YPPF", password: 'brandeis16', password_confirmation: 'brandeis16', id: 1)
 
 5.times do
-  Bill.create(expenseid: Faker::Commerce.department, day_month: Faker::Number.number(2), month: Faker::Number.between(from =1, to =12), year: Faker::Number.between(from = 1901, to=2016), user_id: 1)
+  # Bill.create(expenseid: Faker::Commerce.department, day_month: Faker::Number.number(2), month: Faker::Number.between(from =1, to =12), year: Faker::Number.between(from = 1901, to=2016), user_id: 1)
+  Bill.create(expenseid: Faker::Commerce.department, day_month: Faker::Number.number(2), month: Date.today.month, year: Date.today.year, user_id: 1)
+
 end
 
-5.times do
-  Expense.create(expenseid: Faker::Commerce.department, user_id: 1, expensetype: Faker::Lorem.word, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: Faker::Number.between(from = 1, to =12), year: Faker::Number.between(from = 1901, to=2016))
+10.times do
+  # Expense.create(expenseid: Faker::Commerce.department, user_id: 1, expensetype: Faker::Lorem.word, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: Faker::Number.between(from = 1, to =12), year: Faker::Number.between(from = 1901, to=2016))
+  Expense.create(expenseid: Faker::Commerce.department, user_id: 1, expensetype: Faker::Lorem.word, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: Date.today.month, year: Date.today.year)
+
 end
 
-5.times do
-  Income.create(user_id: 1, income: "Money", incometype: "salary", value: Faker::Commerce.price, frequency: "biweekly", day_rec: Faker::Number.number(2), month: Faker::Number.number(1), year: Faker::Number.between(from = 1901, to=2016))
+10.times do
+  # Income.create(user_id: 1, income: "Money", incometype: "salary", value: Faker::Commerce.price, frequency: "biweekly", day_rec: Faker::Number.number(2), month: Faker::Number.number(1), year: Faker::Number.between(from = 1901, to=2016))
+  Income.create(user_id: 1, income: "Money", incometype: "salary", value: Faker::Commerce.price, frequency: "biweekly", day_rec: Faker::Number.number(2), month: Date.today.month, year: Date.today.year)
+
 end
