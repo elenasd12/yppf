e2a997be1d4d8988c95760956712e9dcd9d85733
Eden Shoshan <eshoshan@brandeis.edu>
2016-03-24 13:47:23 -0400

    edit expense index page bc no expenseid. expense controller limited to current user expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,7 +4,7 @@ class ExpensesController < ApplicationController
   # GET /expenses
   # GET /expenses.json
   def index
-    @expenses = Expense.all
+    @expenses = Expense.where(user_id: current_user.id)
   end
 
   # GET /expenses/1
