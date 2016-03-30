901bd29bbe6b3bc6c248b82fdc124d71fe0dd0c1
Mojir Samimi <mojir.s@gmail.com>
2016-03-30 11:23:56 -0400

    ready for codeship

---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+   test "login to site" do
+    # login via https
+    
+    get "/users/sign_in"
+    assert_response :success
+ 
+    post_via_redirect "/users/sign_in", :user => { :email => 'yppf2016@gmail.com', :password => 'brandeis16'}
+    assert_equal '/', path
+    assert_equal 'Signed in successfully.', flash.first[1]
+ 
+  end
+end
