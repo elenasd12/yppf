47e72c28461f1b68849d9468ff4e94d5c28520ae
Elena <elenasd12@gmail.com>
2016-04-05 16:04:17 -0400

    elena testing ajax

--- app/controllers/trend_controller.rb
+++ app/controllers/trend_controller.rb
@@ -16,6 +16,32 @@ class TrendController < ApplicationController
       @chartarray.append([[y[0][0].to_i, y[0][1].to_i], y[1].to_i])
     }
 
-    puts @chartarray.to_s
+    @expensedropdown = []
+    # for the expense bar chart
+    z = Expense.select(:expense_category_id).where(year: Date.today.year).where(user_id: current_user.id).group(:expense_category_id)
+    z.each {|a|
+      @expensedropdown.append([a.expense_category_name, a.expense_category_id])
+    }
+
+
+    respond_to do |format|
+      format.js
+    end
   end
+
+  def barchart
+    selected_expense = params[:expensedropdown]
+    @selected_expense = ExpenseCategory.find(selected_expense).exp_name
+
+    q = Expense.where(user_id: current_user.id).where(expense_category_id: selected_category).where(year: Date.today.year).group(:month).order(:month).sum(:actvalue)
+
+    @months = []
+    @values = []
+
+    q.each {|x|
+      @months.append(x[0])
+      @values.append(x[1])
+    }
+  end
+
 end
