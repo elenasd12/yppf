657c0390e0a92ce352f7ff13620559544f14dfa8
Elena <elenasd12@gmail.com>
2016-04-20 18:34:33 -0400

    test notification

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def new
-
+    Pusher['test_channel'].trigger('greet', {
+      :greeting => "Hello there!"
+    })
   end
 
 end
