c1c72e142a452045a4ece6d3ef435a869d56e162
Mojir Samimi <mojir.s@gmail.com>
2016-04-30 00:25:31 -0400

    improved budget page

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -4,7 +4,7 @@ module BudgetsHelper
     
     @expenses = Expense.where(user_id: current_user.id).where(month:Date.today.month).where(year:Date.today.year)
     expense_cat_id=@expenses.select(:expense_category_id).distinct
-    @expense_categories = ExpenseCategory.where(id: expense_cat_id)
+    @expense_categories = ExpenseCategory.where(id: expense_cat_id).order(exp_name: :asc)
     
   end
   def initialize_incomes
@@ -15,7 +15,12 @@ module BudgetsHelper
 
   def expenses_by_category(category_id)
 
-    Expense.where(user_id: current_user.id,month:Date.today.month,year:Date.today.year,expense_category_id: category_id)
+    Expense.where(user_id: current_user.id,month:Date.today.month,year:Date.today.year,expense_category_id: category_id).order(expensename: :asc)
+
+  end
+  def expenses_size_by_category(category_id)
+
+    Expense.where(user_id: current_user.id,month:Date.today.month,year:Date.today.year,expense_category_id: category_id).size
 
   end
 end
