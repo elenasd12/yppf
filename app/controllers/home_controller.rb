70fcdccc6b88d952cd2286ba327d5ec6f9acb8d3
Mojir Samimi <mojir.s@gmail.com>
2016-05-02 17:02:53 -0400

    last min changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def new
+    if user_signed_in?
+      redirect_to '/dashboard/new'
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ ExpenseDetail.reset_pk_sequence
 MONTHS = [0, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 TYPE = ['Rent', 'Electric', 'Heat', 'TV', 'Internet', 'Groceries', 'Eating Out', 'Entertainment', 'Clothing', 'Transportation']
 #User
-User.create(email: "yppf2016@gmail.com", state: "MA", name: "YPPF", password: 'brandeis16', password_confirmation: 'brandeis16', id: 1)
+User.create(email: "samtheman@yppf.com", state: "MA", name: "Sam", password: 'brandeis16', password_confirmation: 'brandeis16', id: 1)
 
 #List of Values
 Listofvalue.create(lov_domain: "exp_ref_type",lov_key: 1,lov_value: 'Once')
@@ -41,12 +41,67 @@ end
 (0..11).each do |i|
   Listofvalue.create(lov_domain: "year",lov_key: 2016+i,lov_value: (2016+i).to_s)
 end
+#Expense Categories
+#exp_type 1=Expense, 2=Bill
+(0..9).each do |i|
+ExpenseCategory.create(exp_name: TYPE[i], id: i+1,exp_type: 1)
+end
+ExpenseCategory.create(exp_name: 'Phone Bill', id: 11,exp_type: 2)
 
 #Expense Reference
 #ref_type 1=Once, 2=Every Month
-(0..9).each do |i|
-ExpenseReference.create(ref_name: 'its just a name',id: i+1,user_id: 1, ref_value: Faker::Commerce.price,ref_type: (i%2)+1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 1+i)
+#Rent
+ExpenseReference.create(ref_name: 'Home Rent',id: 1,user_id: 1, ref_value: 1325,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 1)
+ExpenseReference.create(ref_name: 'Home Rent',id: 2,user_id: 1, ref_value: 1325,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 1)
+ExpenseReference.create(ref_name: 'Home Rent',id: 3,user_id: 1, ref_value: 1325,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 1)
+
+#Electric
+ExpenseReference.create(ref_name: 'Home Eversource',id: 4,user_id: 1, ref_value: 40,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 2)
+ExpenseReference.create(ref_name: 'Home Eversource',id: 5,user_id: 1, ref_value: 35,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 2)
+ExpenseReference.create(ref_name: 'Home Eversource',id: 6,user_id: 1, ref_value: 25,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 2)
+#Heat
+ExpenseReference.create(ref_name: 'Home Heat',id: 7,user_id: 1, ref_value: 50,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 3)
+ExpenseReference.create(ref_name: 'Home Heat',id: 8,user_id: 1, ref_value: 45,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 3)
+ExpenseReference.create(ref_name: 'Home Heat',id: 9,user_id: 1, ref_value: 35,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 3)
+#Groceries
+ExpenseReference.create(ref_name: 'Dog Food',id: 10,user_id: 1, ref_value: 20,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 6)
+ExpenseReference.create(ref_name: 'Dog Food',id: 11,user_id: 1, ref_value: 30,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 6)
+ExpenseReference.create(ref_name: 'Dog Food',id: 12,user_id: 1, ref_value: 25,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 6)
+ExpenseReference.create(ref_name: 'Dairy',id: 13,user_id: 1, ref_value: 100,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 6)
+ExpenseReference.create(ref_name: 'Dairy',id: 14,user_id: 1, ref_value: 125,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 6)
+ExpenseReference.create(ref_name: 'Dairy',id: 15,user_id: 1, ref_value: 90,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 6)
+#Eating Out
+ExpenseReference.create(ref_name: 'Restaurant',id: 16,user_id: 1, ref_value: 50,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 7)
+ExpenseReference.create(ref_name: 'Restaurant',id: 17,user_id: 1, ref_value: 60,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 7)
+ExpenseReference.create(ref_name: 'Restaurant',id: 18,user_id: 1, ref_value: 70,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 7)
+ExpenseReference.create(ref_name: 'Coffee Shop',id: 19,user_id: 1, ref_value: 25,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 7)
+ExpenseReference.create(ref_name: 'Coffee Shop',id: 20,user_id: 1, ref_value: 30,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 7)
+ExpenseReference.create(ref_name: 'Coffee Shop',id: 21,user_id: 1, ref_value: 25,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 7)
+#Entertainment
+ExpenseReference.create(ref_name: 'Cinema',id: 22,user_id: 1, ref_value: 10,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 8)
+ExpenseReference.create(ref_name: 'Cinema',id: 23,user_id: 1, ref_value: 20,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 8)
+ExpenseReference.create(ref_name: 'Cinema',id: 24,user_id: 1, ref_value: 15,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 8)
+ExpenseReference.create(ref_name: 'Museum',id: 25,user_id: 1, ref_value: 20,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 8)
+ExpenseReference.create(ref_name: 'Museum',id: 26,user_id: 1, ref_value: 35,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 8)
+ExpenseReference.create(ref_name: 'Museum',id: 27,user_id: 1, ref_value: 25,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 8)
+#Clothing
+ExpenseReference.create(ref_name: 'Sport Cloths',id: 28,user_id: 1, ref_value: 50,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 9)
+ExpenseReference.create(ref_name: 'Sport Cloths',id: 29,user_id: 1, ref_value: 100,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 9)
+ExpenseReference.create(ref_name: 'Sport Cloths',id: 30,user_id: 1, ref_value: 75,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 9)
+ExpenseReference.create(ref_name: 'Casual Cloths',id: 31,user_id: 1, ref_value: 60,ref_type: 1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 9)
+ExpenseReference.create(ref_name: 'Casual Cloths',id: 32,user_id: 1, ref_value: 50,ref_type: 1,ref_status: 1,ref_month: 4,ref_year: Date.today.year,expense_category_id: 9)
+ExpenseReference.create(ref_name: 'Casual Cloths',id: 33,user_id: 1, ref_value: 55,ref_type: 1,ref_status: 1,ref_month: 5,ref_year: Date.today.year,expense_category_id: 9)
+
+(1..33).each do |i|
+  exp1=Expense.where(user_id: 1,expense_reference_id: i)
+  
+  exp1.each do |exp|
+  ExpenseDetail.create(expdet_date: DateTime.new(exp.year, exp.month, 1),expdet_value: rand(5..30),user_id: 1,expense_id:exp.id,expdet_description: "it was fun.")
+  end
 end
+#(0..9).each do |i|
+#ExpenseReference.create(ref_name: 'its just a name',id: i+1,user_id: 1, ref_value: Faker::Commerce.price,ref_type: (i%2)+1,ref_status: 1,ref_month: 3,ref_year: Date.today.year,expense_category_id: 1+i)
+#end
 
 5.times do
   # Bill.create(expenseid: Faker::Commerce.department, day_month: Faker::Number.number(2), month: Faker::Number.between(from =1, to =12), year: Faker::Number.between(from = 1901, to=2016), user_id: 1)
@@ -55,28 +110,23 @@ end
 end
 
 
-#Expense Categories
-#exp_type 1=Expense, 2=Bill
-(0..9).each do |i|
-ExpenseCategory.create(exp_name: TYPE[i], id: i+1,exp_type: 1)
-end
-ExpenseCategory.create(exp_name: 'Phone Bill', id: 11,exp_type: 2)
+
 
 #Expenses
-(0..9).each do |i|
-  Expense.create(expense_reference_id: 1,user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 1, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 2, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 3, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 4, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 5, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 6, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 7, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 8, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 9, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 10, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 11, year: Date.today.year)
-  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 12, year: Date.today.year)
-end
+#(0..9).each do |i|
+#  Expense.create(expense_reference_id: 1,user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 1, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 2, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 3, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 4, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 5, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 6, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 7, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 8, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 9, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 10, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 11, year: Date.today.year)
+#  Expense.create(expensename: "General",user_id: 1, expense_category_id: 1+i, expensetype: 0, frequency: "biweekly", projvalue: Faker::Commerce.price, actvalue: Faker::Commerce.price, percent: Faker::Number.number(2), month: 12, year: Date.today.year)
+#end
 
 
 5.times do
