e8b1c4bba409724cf56d55e96ccb51e87cdd1771
Eden Shoshan <eshoshan@brandeis.edu>
2016-05-02 17:10:05 -0400

    get rid of hello there pusher popup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def new
-    Pusher['test_channel'].trigger('greet', {
-      :greeting => "Hello there!"
-    })
+    # Pusher['test_channel'].trigger('greet', {
+    #   :greeting => "Hello there!"
+    # })
   end
 
 end
